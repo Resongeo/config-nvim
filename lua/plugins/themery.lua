7b6e318f982735692db7ca8bc0780e80c30f6519
Resongeo <s.benedek2003@gmail.com>
2025-06-26 15:55:43 +0200

    fix: Remove unused themes from Themery

--- lua/plugins/themery.lua
+++ lua/plugins/themery.lua
@@ -2,17 +2,14 @@ return {
     "zaldih/themery.nvim",
     lazy = false,
     config = function()
-      require("themery").setup({
-        themes = {
+        require("themery").setup({
+            themes = {
                 {
                     name = "Catppuccin Custom",
                     colorscheme = "catppuccin-macchiato",
                 },
-                "kanagawa-wave",
-                "evergarden",
-                "oldworld",
             }, 
-        livePreview = true
-      })
+            livePreview = true
+        })
     end
 }
