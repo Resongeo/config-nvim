714170c99da63139ca27db068d1f55ca1335501c
Resongeo <s.benedek2003@gmail.com>
2025-07-12 17:04:05 +0200

    feat: Add harpoon plugin

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -20,3 +20,23 @@ vim.keymap.set('v', 'y', '"+y')
 -- Telescope
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+
+-- Harpoon
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
+
+vim.keymap.set('n', '<leader>a', mark.add_file)
+vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu)
+
+vim.keymap.set('n', '<leader>1', function()
+   ui.nav_file(1)
+end)
+vim.keymap.set('n', '<leader>2', function()
+   ui.nav_file(2)
+end)
+vim.keymap.set('n', '<leader>3', function()
+   ui.nav_file(3)
+end)
+vim.keymap.set('n', '<leader>4', function()
+   ui.nav_file(4)
+end)
