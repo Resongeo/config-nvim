0f7073b5796ec796981a9b21913e6dd9ac345583
Resongeo <s.benedek2003@gmail.com>
2026-03-11 15:43:14 +0100

    feat: format options for lua and json

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -3,10 +3,20 @@ return {
     opts = {
         formatters_by_ft = {
             cpp = { "clang-format" },
+            lua = { "stylua" },
+            json = { "fixjson" },
         },
         format_on_save = {
-            timeout_ms = 500,
+            timeout_ms = 2000,
             lsp_format = "fallback",
         },
+        formatters = {
+            stylua = {
+                append_args = { "--indent-width", "3", "--indent-type", "Spaces" }
+            },
+            fixjson = {
+                append_args = { "-i", "2" }
+            }
+        }
     },
 }
