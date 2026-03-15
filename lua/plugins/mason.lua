aba46d17909a3282c3b9eb625349edba3a7bf137
Resongeo <s.benedek2003@gmail.com>
2026-03-15 19:22:43 +0100

    feat: add ensure installed list to Mason

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,13 +1,23 @@
 return {
-    "mason-org/mason.nvim",
-    opts = {
-        ui = {
-            icons = {
-                package_installed = "✓",
-                package_pending = "➜",
-                package_uninstalled = "✗"
-            },
-            border = "rounded",
-        }
-    }
+   "mason-org/mason.nvim",
+   opts = {
+      ui = {
+         icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+         },
+         border = "rounded",
+      },
+      ensure_installed = {
+         "basdepyright",
+         "clangd",
+         "ols",
+         "ruff",
+         "rust-analyzer",
+         "stylua",
+         "tombi",
+         "tsgo",
+      },
+   },
 }
